37fb9acca8d6e8dd9218f1f20202c42e4fb48c43
‘dianprasetyoo’ <dipra94@gmail.com>
2023-11-04 19:58:31 +0700

    fixing images

--- next.config.js
+++ next.config.js
@@ -4,6 +4,8 @@ const images = require("next-images");
 module.exports = images({
   images: {
     domains: ["via.placeholder.com"], // whitelist host
+    loader: 'imgix', // atau gunakan 'default' jika lebih sesuai
+    path: '/_next/image', // atau sesuaikan path berdasarkan kebutuhan Anda
   },
   basePath: "", // Tambahkan basePath kosong untuk deploy ke root domain
   assetPrefix: "", // Tambahkan assetPrefix kosong untuk deploy ke root domain
